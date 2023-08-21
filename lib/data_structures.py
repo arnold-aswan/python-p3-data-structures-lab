5badee68b25bb6087771ac2a64bc4daf38116cb5
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-08-21 11:09:42 +0300

    refactored code to use list comprehension

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,44 +17,32 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-   #return [foods['name'] for foods in spicy_foods ]
-    food =[]
-    for foods in spicy_foods:
-       food.append(foods['name'])
-    return food   
+    return [foods["name"] for foods in spicy_foods]
        
 def get_spiciest_foods(spicy_foods):
-    #return [foods for foods in spicy_foods if foods['heat_level'] > 5]
-    food = []
-    for foods in spicy_foods:
-        if foods['heat_level'] > 5:
-            food.append(foods)
-        return food    
+    return [foods for foods in spicy_foods if foods["heat_level"] > 5]
 
 def print_spicy_foods(spicy_foods):
-    for foods in spicy_foods:
-        heat_level = foods['heat_level']
-        chilli = "🌶" * heat_level
-        print(f"{foods['name']} ({foods['cuisine']}) | Heat Level: {chilli}")
-
+    spicy = [f"{foods['name']} ({foods['cuisine']}) | Heat Level: {'🌶' * foods['heat_level']}" for foods in spicy_foods ]
+    print(*spicy, sep='\n')
+    
+    
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     for foods in spicy_foods:
         if cuisine.lower() == foods['cuisine'].lower():
             return foods
 
 def print_spiciest_foods(spicy_foods):
-    for foods in spicy_foods:
-        heat_level = foods['heat_level']
-        chilli = "🌶" * heat_level
-        if foods['heat_level'] > 5:
-            print(f"{foods['name']} ({foods['cuisine']}) | Heat Level: {chilli}")
+    spicy = [f"{foods['name']} ({foods['cuisine']}) | Heat Level: {'🌶' * foods['heat_level']}" for foods in spicy_foods if foods['heat_level'] > 5]
+    print(*spicy, sep="\n")
 
 def get_average_heat_level(spicy_foods):
     sum = 0
     for foods in spicy_foods:
         sum = sum + foods['heat_level']
-        avg = sum / len(foods)
+    avg = sum / len(foods)
     return avg
+   
 
 def create_spicy_food(spicy_foods, spicy_food):
     spicy_foods.append(spicy_food)
